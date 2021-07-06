9eef76c351924dc153bf8fa533942816e42bc44c
Mahmudulla Hassan <mhassan@miners.utep.edu>
2021-07-05 22:23:05 -0600

    add resnet, mean and std calculation and modify checkpoint saving

--- xray/train.py
+++ xray/train.py
@@ -1,7 +1,7 @@
 import json
-import multiprocessing as mp
 import os
 from argparse import ArgumentParser
+from collections import OrderedDict
 
 import pandas as pd
 import torch
@@ -9,11 +9,10 @@ import torch.optim as optim
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms, models
 
-from .dataset import XrayImageDataset
-from .models import BaselineModel, ModelOne, ModelTwo
-
-writer = SummaryWriter("./test")
+from xray.dataset import XrayImageDataset
+from xray.models import ModelTwo, BaselineModel
 
 seed = 42
 torch.manual_seed(seed)
@@ -32,37 +31,93 @@ def binary_acc(y_pred, y_test):
 
 
 def save_checkpoint(state, is_best, filename='./output/checkpoint.pth.tar'):
+    weight_dir = os.path.dirname(filename)
     """Save checkpoint if a new best is achieved"""
     if is_best:
         print("=> Saving a new best")
-        torch.save(state, filename)  # save checkpoint
+        torch.save(state, os.path.join(weight_dir, "checkpoint-best.pth.tar"))  # save checkpoint
     else:
         print("=> Validation Accuracy did not improve")
+    torch.save(state, filename)
+
+
+def get_mean_std(loader):
+    channels_sum, channels_squared_sum, num_batches = 0, 0, 0
+
+    for data, _ in loader:
+        channels_sum += torch.mean(data, dim=(0, 2, 3))
+        channels_squared_sum += torch.mean(data ** 2, dim=(0, 2, 3))
+        num_batches += 1
+
+    mean = channels_sum / num_batches
+    std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
+
+    return mean, std
 
 
 def train(args, evaluate_only=True):
+    writer = SummaryWriter(args["model_dir"])
     dataset_dir = args['dataset_dir']
-    train_dir = os.path.join(dataset_dir, 'train-set')
-    valid_dir = os.path.join(dataset_dir, 'validation-set')
-    test_dir = os.path.join(dataset_dir, 'test-set')
-
     batch_size = args['batch_size']
     epochs = args['epochs']
     learning_rate = args['learning_rate']
     num_workers = args['num_worker']
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    training_data = XrayImageDataset(os.path.join(dataset_dir, 'train-labels-short.csv'), train_dir)
-    validation_data = XrayImageDataset(os.path.join(dataset_dir, 'validation-labels-short.csv'), valid_dir)
-    test_data = XrayImageDataset(os.path.join(dataset_dir, 'sample-submission.csv'), test_dir, True)
+    # Find the mean and std of training data.
+    # Uncomment the following block to compute mean and std and comment the hardcoded values after.
+    # train_data = XrayImageDataset(
+    #     annotations_file=os.path.join(dataset_dir, 'train-labels.csv'),
+    #     img_dir=os.path.join(dataset_dir, 'train-set'),
+    #     transform=transforms.Compose(
+    #         [
+    #             transforms.Resize((256, 256)),
+    #             transforms.ToTensor()
+    #         ]
+    #     )
+    # )
+    #
+    # train_loader = DataLoader(
+    #     train_data,
+    #     batch_size=512,
+    #     shuffle=False,
+    #     num_workers=num_workers,
+    #     pin_memory=True
+    # )
+    #
+    # mean, std = get_mean_std(train_loader)
+    mean = torch.Tensor([0.7165, 0.7446, 0.7119])
+    std = torch.Tensor([0.3062, 0.2433, 0.2729])
+    transform = transforms.Compose(
+        [
+            # transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=mean,
+                std=std
+            )
+        ]
+    )
+    train_data = XrayImageDataset(
+        annotations_file=os.path.join(dataset_dir, 'train-labels.csv'),
+        img_dir=os.path.join(dataset_dir, 'train-set'),
+        transform=transform
+    )
 
     train_loader = DataLoader(
-        training_data,
+        train_data,
         batch_size=batch_size,
         shuffle=True,
         num_workers=num_workers,
         pin_memory=True
     )
+
+    validation_data = XrayImageDataset(
+        annotations_file=os.path.join(dataset_dir, 'validation-labels.csv'),
+        img_dir=os.path.join(dataset_dir, 'validation-set'),
+        transform=transform
+    )
+
     validation_loader = DataLoader(
         validation_data,
         batch_size=batch_size,
@@ -70,6 +125,13 @@ def train(args, evaluate_only=True):
         num_workers=num_workers,
         pin_memory=True
     )
+
+    test_data = XrayImageDataset(
+        annotations_file=os.path.join(dataset_dir, 'sample_submission.csv'),
+        img_dir=os.path.join(dataset_dir, 'test-set'),
+        transform=transform
+    )
+
     test_loader = DataLoader(
         test_data,
         batch_size=batch_size,
@@ -77,8 +139,53 @@ def train(args, evaluate_only=True):
         num_workers=num_workers,
         pin_memory=False
     )
+    model = None
+    if args["model_name"] == "baseline":
+        model = BaselineModel()
+    if args["model_name"] == "ModelTwo":
+        model = ModelTwo()
+    elif args["model_name"] == "resnet34":
+        model = models.resnet34(pretrained=args["pretrained"])
 
-    model = BaselineModel()
+        if args["pretrained"]:
+            for param in model.parameters():
+                param.requires_grad = False
+
+        layers_resnet = nn.Sequential(OrderedDict([
+            ('dropout1', nn.Dropout(0.5)),
+            ('fc1', nn.Linear(512, 256)),
+            ('activation1', nn.ReLU()),
+            ('dropout2', nn.Dropout(0.3)),
+            ('fc2', nn.Linear(256, 128)),
+            ('activation2', nn.ReLU()),
+            ('fc3', nn.Linear(128, 1))
+            # ('out', nn.Sigmoid())
+        ]))
+
+        model.fc = layers_resnet
+    elif args["model_name"] == "vgg19_bn":
+        model = models.vgg19_bn(pretrained=args["pretrained"])
+
+        if args["pretrained"]:
+            for param in model.parameters():
+                param.requires_grad = False
+
+        layers = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(25088, 4096)),
+            ('activation1', nn.ReLU()),
+            ('dropout1', nn.Dropout(0.5)),
+            ('fc2', nn.Linear(4096, 128)),
+            ('activation2', nn.ReLU()),
+            ('dropout2', nn.Dropout(0.3)),
+            ('fc3', nn.Linear(128, 1))
+            # ('out', nn.Sigmoid())
+        ]))
+
+        model.classifier = layers
+
+    assert (model is not None)
+    print(model)
+
     if torch.cuda.device_count() > 1:
         print("Using ", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
@@ -109,6 +216,7 @@ def train(args, evaluate_only=True):
     criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    best_accuracy = 0.0
     for epoch in range(1, epochs + 1):  # loop over the dataset multiple times
         model.train()
         epoch_loss = 0.0
@@ -143,7 +251,6 @@ def train(args, evaluate_only=True):
         model.eval()
         val_accuracy = 0.0
         val_loss = 0.0
-        best_accuracy = 0.0
         with torch.no_grad():
             for i, (x, y) in enumerate(validation_loader, 0):
                 inputs, labels = x.to(device), y.to(device).unsqueeze(1)
@@ -154,10 +261,10 @@ def train(args, evaluate_only=True):
                 val_accuracy += acc.item()
                 val_loss += loss.item()
 
-        acc = val_accuracy / len(validation_loader)
+        val_accuracy = val_accuracy / len(validation_loader)
         val_loss = val_loss / len(validation_loader)
-        is_best = bool(acc > best_accuracy)
-        best_accuracy = max(acc, best_accuracy)
+        is_best = bool(val_accuracy > best_accuracy)
+        best_accuracy = max(val_accuracy, best_accuracy)
         # Save checkpoint if is a new best
         save_checkpoint(
             {
@@ -168,7 +275,7 @@ def train(args, evaluate_only=True):
                 'best_accuracy': best_accuracy
             },
             is_best,
-            filename=os.path.join(args['model_dir'], 'checkpoint.pth.tar')
+            filename=os.path.join(args['model_dir'], f'checkpoint-{epoch:03d}-val-{val_accuracy:.3f}.pth.tar')
         )
 
         train_loss = epoch_loss / len(train_loader)
@@ -195,7 +302,7 @@ def main():
                         help="JSON input")
     args = parser.parse_args()
     if not os.path.isfile(args.input):
-        raise FileNotFoundError("Input {args.input} not found.")
+        raise FileNotFoundError(f"Input {args.input} not found.")
 
     with open(args.input) as f:
         args = json.load(f)
